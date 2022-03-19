c7852e496e5c0767a7faab32fd63cd153b8fdeee
brianmwevi <mwevibrian@gmail.com>
2022-03-19 22:35:56 +0300

    ft: Order pricing method -- create a prototype to get total order cost

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,36 @@
 $(document).ready(function () {
 	$(".pizza-form").submit(validateForm);
 });
+let countOrders = 0;
+
+// Prices
+const price = {
+	size: {
+		small: 300,
+		medium: 500,
+		large: 800,
+	},
+	crust: {
+		stuffed: 100,
+		crispy: 150,
+		gf: 200,
+	},
+	delivery: 150,
+	pickup: 0,
+	toppings: {
+		pepperoni: 80,
+		onions: 30,
+		tomatoes: 40,
+		chicken: 100,
+	},
+	toppingsPrice: function (toppingsArray) {
+		let cost = 0;
+		for (const topping of toppingsArray) {
+			cost += this.toppings[topping];
+		}
+		return cost;
+	},
+};
 
 // Display Form Errors
 const formError = (error) => {
@@ -16,14 +46,23 @@ const validateForm = (submit) => {
 	submit.preventDefault();
 	let toppings = $("input[type='checkbox']:checked");
 	if (toppings.length === 0) return formError("Please select topping(s)");
-	const dispatch = $("input[name='dispatch']:checked");
+	const dispatch = $("input[name='dispatch']:checked").val();
 	if (dispatch.length === 0) return formError("Please select dispatch method");
 	const pizzaSize = $("#pizza-size").val();
 	const pizzaCrust = $("#pizza-crust").val();
 	toppings = toppings.map((topping) => toppings[topping].value);
-	console.log(new Order(pizzaSize, pizzaCrust, toppings, dispatch));
+	const order = new Order(pizzaSize, pizzaCrust, toppings, dispatch);
+	Order.prototype.cost = caclCost;
+	console.log(order.cost());
 };
-
+const caclCost = function () {
+	return (
+		price.size[this.size] +
+		price.crust[this.crust] +
+		price.toppingsPrice(this.toppings) +
+		price[this.dispatch]
+	);
+};
 // Construct a New Order
 const Order = function (size, crust, toppings, dispatch) {
 	this.size = size;
@@ -31,6 +70,3 @@ const Order = function (size, crust, toppings, dispatch) {
 	this.toppings = toppings;
 	this.dispatch = dispatch;
 };
-
-
-// Display Order List
