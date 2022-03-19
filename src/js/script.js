789612de210f6b350c936e00910f390e857a5f7d
brianmwevi <mwevibrian@gmail.com>
2022-03-20 00:18:35 +0300

    ft: order list -- define a function to display order list

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,13 @@
 $(document).ready(function () {
 	$(".pizza-form").submit(validateForm);
+	$(".checkout").click(showOrderList)
 });
+
 let countOrders = 0;
-// Prices
+let totalCost = 0;
+let orderList = [];
+
+// Price Object
 const price = {
 	size: { small: 300, medium: 500, large: 800 },
 	crust: { stuffed: 100, crispy: 150, gf: 200 },
@@ -27,7 +32,6 @@ const formError = (error) => {
 	});
 };
 
-// Validate Form
 const validateForm = (submit) => {
 	submit.preventDefault();
 	let toppings = $("input[type='checkbox']:checked");
@@ -36,35 +40,56 @@ const validateForm = (submit) => {
 	if (dispatch.length === 0) return formError("Please select dispatch method");
 	const pizzaSize = $("#pizza-size").val();
 	const pizzaCrust = $("#pizza-crust").val();
-	toppings = toppings.map((topping) => toppings[topping].value);
-	return displayOrder(new Order(pizzaSize, pizzaCrust, toppings, dispatch));
+	toppings = toppings.map((index, input) => input.value);
+	return orderSummary(new Order(pizzaSize, pizzaCrust, toppings, dispatch));
 };
 
-const displayOrder = (order) => {
-	$(".cart-items").text(countOrders += 1);
+// Display latest Order Summary
+const orderSummary = (order) => {
+	$(".cart-count").text((countOrders += 1));
 	$(".pizza-size").text(order.size);
 	$(".pizza-crust").text(order.crust);
 	$(".pizza-toppings").text(price.toppingsPrice[order.toppings]);
 	$(".dispatch-cost").text(price[order.dispatch]);
 	$(".dispatch").text(order.dispatch);
 	$(".pizza-cost").text(order.getCost());
-	
-}
+	let toppings = [];
+	for (let topping of order.toppings) {
+		toppings.push(topping);
+	}
+	orderList.push(`
+	<tbody>
+		<tr>
+			<td scope="row">${countOrders}</td>
+			<td>${order.size}</td><td>${order.crust}</td>
+			<td>${toppings}</td>
+			<td>Ksh ${order.getCost()}</td>
+		</tr>
+	</tbody>`);
+};
 
+// Order Prototype method
 const calcCost = function () {
 	return (
 		price.size[this.size] +
 		price.crust[this.crust] +
 		price.toppingsPrice(this.toppings) +
 		price[this.dispatch]
-		);
-	};
-	// Construct a New Order
-	const Order = function (size, crust, toppings, dispatch) {
-		this.size = size;
-		this.crust = crust;
+	);
+};
+
+// New Order Constructor
+const Order = function (size, crust, toppings, dispatch) {
+	this.size = size;
+	this.crust = crust;
 	this.toppings = toppings;
 	this.dispatch = dispatch;
 };
 
 Order.prototype.getCost = calcCost;
+
+const showOrderList = () => {
+	
+	$(".cart-items tbody").remove()
+	orderList.forEach((order) => $(".cart-items").append(order));
+};
