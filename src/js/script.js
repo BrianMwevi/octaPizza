fa08aa8044ce15e7eb8caf61b337edb2b615d876
brianmwevi <mwevibrian@gmail.com>
2022-03-19 19:18:48 +0300

    ft: Order constructor -- create new order constructor

--- src/js/script.js
+++ src/js/script.js
@@ -14,11 +14,23 @@ const formError = (error) => {
 // Validate Form
 const validateForm = (submit) => {
 	submit.preventDefault();
-	const toppings = $("input[type='checkbox']:checked");
+	let toppings = $("input[type='checkbox']:checked");
 	if (toppings.length === 0) return formError("Please select topping(s)");
 	const dispatch = $("input[name='dispatch']:checked");
 	if (dispatch.length === 0) return formError("Please select dispatch method");
+	const pizzaSize = $("#pizza-size").val();
+	const pizzaCrust = $("#pizza-crust").val();
+	toppings = toppings.map((topping) => toppings[topping].value);
+	console.log(new Order(pizzaSize, pizzaCrust, toppings, dispatch));
 };
 
-// Create order request
-const Order = function ()
+// Construct a New Order
+const Order = function (size, crust, toppings, dispatch) {
+	this.size = size;
+	this.crust = crust;
+	this.toppings = toppings;
+	this.dispatch = dispatch;
+};
+
+
+// Display Order List
