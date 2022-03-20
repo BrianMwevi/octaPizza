e47afb0a58839f4db657064ec2d5f4973f15101b
brianmwevi <mwevibrian@gmail.com>
2022-03-20 20:36:52 +0300

    patch: validate location form and add checkout page animation

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,33 @@
 $(document).ready(function () {
+	Order.prototype.getCosts = calcCost;
 	$(".pizza-form").submit(validateForm);
-	$(".checkout").click(showOrderList);
+	$(".back-home, .back-to-cart").click(function () {
+		if (this.classList.contains("back-home")) {
+			$(".pizza-order").show("slow");
+		} else {
+			$(".pizza-orderlist").show("slow");
+		}
+		$(".checkout-modal, .checkout-container, .feedback-container").hide("slow");
+	});
+	$(".view-cart,.back").click(function () {
+		$(".pizza-orderlist, .pizza-order").slideToggle("slow");
+	});
+	$(".place-order").click(placeOrder);
+	$(".checkout").click(function () {
+		for (const order of ordersArray) {
+			if (order.dispatch === "delivery" && !order.location) {
+				return $(
+					".checkout-modal, .checkout-container, .pizza-orderlist"
+				).slideToggle("slow");
+			}
+		}
+		$(".checkout-modal, .feedback-container, .pizza-orderlist").slideToggle(
+			"slow"
+		);
+	});
 });
 
-let countOrders = 0;
-let totalCost = 0;
-let orderList = [];
+let ordersArray = [];
 
 // Price Object
 const price = {
@@ -13,71 +35,65 @@ const price = {
 	crust: { stuffed: 100, crispy: 150, gf: 200 },
 	delivery: 150,
 	pickup: 0,
-	toppings: { pepperoni: 80, onions: 30, tomatoes: 40, chicken: 100 },
-	toppingsPrice: function (toppingsArray) {
-		let cost = 0;
-		for (const topping of toppingsArray) {
-			cost += this.toppings[topping];
+	toppings: { cheese: 50, onions: 30, tomatoes: 40, chicken: 100 },
+	totalCost: 0,
+	order: function (order) {
+		let toppings = 0;
+		let eachToppingCost = [];
+		for (const topping of order.toppings) {
+			toppings += this.toppings[topping];
+			eachToppingCost.push(`${topping}: ksh ${this.toppings[topping]}`);
 		}
-		return cost;
+		const pizza = this.size[order.size] + this.crust[order.crust] + toppings;
+		const total = pizza + this[order.dispatch];
+		this.totalCost += total;
+		return [total, pizza, [toppings, eachToppingCost], this[order.dispatch]];
 	},
 };
 
 // Display Form Errors
 const formError = (error) => {
 	$(".error").text(error);
-	$(".error-modal").slideDown("fast");
+	$(".error-modal").slideDown("slow");
 	$(".close-error,.error-overlay").click(function () {
-		$(".error-modal").hide("fast");
+		return $(".error-modal").hide("fast");
 	});
 };
 
 const validateForm = (submit) => {
 	submit.preventDefault();
-	let toppings = $("input[type='checkbox']:checked");
-	if (toppings.length === 0) return formError("Please select topping(s)");
+	let toppingInputs = $("input[type='checkbox']:checked");
+	if (toppingInputs.length === 0) return formError("Please select topping(s)");
 	const dispatch = $("input[name='dispatch']:checked").val();
-	if (dispatch.length === 0) return formError("Please select dispatch method");
+	if (dispatch.length === 0) return formError("Please select dispatch process");
 	const pizzaSize = $("#pizza-size").val();
 	const pizzaCrust = $("#pizza-crust").val();
-	toppings = toppings.map((index, input) => input.value);
-	$(".checkout").slideDown("slow");
-	$(".pizza-form").trigger("reset")
-	return orderSummary(new Order(pizzaSize, pizzaCrust, toppings, dispatch));
+	const toppings = toppingInputs.map((i, input) => input.value);
+	$(".view-cart").slideDown("slow");
+	$(".pizza-form").trigger("reset");
+
+	return orderList(new Order(pizzaSize, pizzaCrust, toppings, dispatch));
 };
 
 // Display latest Order Summary
-const orderSummary = (order) => {
-	$(".cart-count").text((countOrders += 1));
-	$(".pizza-size").text(order.size);
-	$(".pizza-crust").text(order.crust);
-	$(".pizza-toppings").text(price.toppingsPrice[order.toppings]);
-	$(".dispatch-cost").text(price[order.dispatch]);
+const orderList = (order) => {
+	ordersArray.push(order);
+	alertPlacedOrder();
+	const orderItem = `
+		<tr class="order-item">
+		<td class="text-capitalize">${order.orderNumber}</td>
+			<td class="text-capitalize">${order.size}: ksh ${price.size[order.size]}</td>
+			<td class="text-capitalize">${order.crust}: ksh ${price.crust[order.crust]}</td>
+			<td class="text-capitalize">${order.cost.toppings[1]}</td>
+			<td class="text-capitalize">Ksh ${order.cost.pizza}</td>
+		</tr>`;
+	$(".cart-count").text(ordersArray.length);
 	$(".dispatch").text(order.dispatch);
-	$(".pizza-cost").text(order.getCost());
-	let toppings = [];
-	for (let topping of order.toppings) {
-		toppings.push(topping);
-	}
-	orderList.push(`
-	<tbody>
-		<tr>
-			<td scope="row">${countOrders}</td>
-			<td>${order.size}</td><td>${order.crust}</td>
-			<td>${toppings}</td>
-			<td>Ksh ${order.getCost()}</td>
-		</tr>
-	</tbody>`);
-};
-
-// Order Prototype method
-const calcCost = function () {
-	return (
-		price.size[this.size] +
-		price.crust[this.crust] +
-		price.toppingsPrice(this.toppings) +
-		price[this.dispatch]
-	);
+	$(".dispatch-cost").text(order.cost.dispatch);
+	$(".pizza-cost").text(order.cost.pizza);
+	$(".cart-items").append(orderItem);
+	$(".total-cost").text(order.cost.total);
+	$(".grand-total").text(price.totalCost);
 };
 
 // New Order Constructor
@@ -86,12 +102,34 @@ const Order = function (size, crust, toppings, dispatch) {
 	this.crust = crust;
 	this.toppings = toppings;
 	this.dispatch = dispatch;
+	this.orderTime = new Date();
+	this.orderNumber = (Math.random() * 1000000).toFixed();
+	this.cost = this.getCosts();
 };
 
-Order.prototype.getCost = calcCost;
+// Cost Prototype
+const calcCost = function () {
+	let cost = {};
+	[cost.total, cost.pizza, cost.toppings, cost.dispatch] = price.order(this);
+	return cost;
+};
 
-const showOrderList = () => {
-	$(".cart-items tbody").remove();
-	$(".pizza-orderlist, .pizza-order").slideToggle("slow");
-	orderList.forEach((order) => $(".cart-items").append(order));
+// Get Location Details
+const placeOrder = () => {
+	const location = $("#location").val();
+	if (location === "") return formError("Please enter delivery location");
+	if (location.length < 8) return formError("Location name is too short");
+	Order.prototype.location = location;
+	$(".dispatch-location").text(location);
+	$(".checkout-container, .feedback-container").slideToggle("slow");
+};
+
+const alertPlacedOrder = () => {
+	$(".error-overlay").css("z-index", "0");
+	formError("Order placed successfully. Proceed to cart to finalize it");
+	let timer = setTimeout(() => {
+		return $(".error-overlay").css("z-index", "3");
+		clearTimeout(timer);
+	}, 3000);
+	return $(".error-modal").slideUp("slow");
 };
